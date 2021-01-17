7fdadd9a5ffcc2b2d0123b5e05194b9642c76870
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-01-17 20:57:04 +0900

    mit

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -22,11 +22,32 @@ var resource = {
 	"description": "This data has been protected by OAuth 2.0"
 };
 
-var getAccessToken = function(req, res, next) {
+var getAccessToken = function (req, res, next) {
 	/*
 	 * Scan for an access token on the incoming request.
 	 */
-	
+	var inToken = null;
+	var auth = req.headers['authorization'];
+	if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
+		inToken = auth.slice('bearer '.length);
+	} else if (req.body && req.body.access_token) {
+		inToken = req.body.access_token;
+	} else if (req.query && req.query.access_token) {
+		inToken = req.query.access_token;
+	}
+	nosql.one().make(function(builder) {
+	  builder.where('access_token', inToken);
+	  builder.callback(function(err, token) {
+	    if (token) {
+	      console.log("We found a matching token: %s", inToken);
+	    } else {
+	      console.log('No matching token was found.');
+	    };
+	    req.access_token = token;
+	    next();
+	    return;
+	  });
+	});
 };
 
 app.options('/resource', cors());
@@ -35,18 +56,22 @@ app.options('/resource', cors());
 /*
  * Add the getAccessToken function to this handler
  */
-app.post("/resource", cors(), function(req, res){
-
+app.post("/resource", cors(), getAccessToken, function (req, res) {
 	/*
 	 * Check to see if the access token was found or not
 	 */
-	
+	if (req.access_token) {
+		res.json(resource);
+	} else {
+		res.status(401).end();
+	}
+
 });
 
 var server = app.listen(9002, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
+	console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
 });
- 
+
