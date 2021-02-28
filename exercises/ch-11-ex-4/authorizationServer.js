fef4d48b38904d90940671ad574304331c16aa53
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-02-28 18:15:02 +0900

    11-4

--- exercises/ch-11-ex-4/authorizationServer.js
+++ exercises/ch-11-ex-4/authorizationServer.js
@@ -81,7 +81,8 @@ var getClient = function(clientId) {
 };
 
 var getProtectedResource = function(resourceId) {
-	return __.find(protectedResources, function(protectedResource) { return protectedResource.resource_id == resourceId; });
+	return __.find(protectedResources, function(protectedResource) { 
+		return protectedResource.resource_id == resourceId; });
 };
 
 var getUser = function(username) {
@@ -286,7 +287,48 @@ app.post('/introspect', function(req, res) {
 	/*
 	 * Implement the introspection endpoint
 	 */
-
+	var auth = req.headers['authorization'];
+	var resourceCredentials = decodeClientCredentials(auth);
+	var resourceId = resourceCredentials.id;
+	var resourceSecret = resourceCredentials.secret;
+	var resource = getProtectedResource(resourceId);
+	if(!resource) {
+		res.status(401).end();
+		return;
+	}
+	if(resource.resource_secret != resourceSecret) {
+		res.status(401).end();
+	}
+	var inToken = req.body.token;
+	nosql.one().make(function(builder) {
+		builder.where('access_token', inToken);
+		builder.callback(function(err, token) {
+			if (token) {
+				console.log("We found a matching token: %s", inToken);
+			  
+				var introspectionResponse = {
+					active: true,
+					iss: 'http://localhost:9001/',
+					aud: 'http://localhost:9002/',
+					sub: token.user ? token.user.sub : undefined,
+					username: token.user ? token.user.preferred_username : undefined,
+					scope: token.scope ? token.scope.join(' ') : undefined,
+					client_id: token.client_id
+				};
+						  
+				res.status(200).json(introspectionResponse);
+				return;
+			} else {
+				console.log('No matching token was found.');
+  
+				var introspectionResponse = {
+					active: false
+				};
+				res.status(200).json(introspectionResponse);
+				return;
+			}
+		})
+	});
 });
 
 var buildUrl = function(base, options, hash) {
